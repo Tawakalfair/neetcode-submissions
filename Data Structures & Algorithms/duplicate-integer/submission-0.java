1d0a056a6ede5c88b5fc91325e526901eaf98a04
Tawakalfair <fairtawakal@gmail.com>
2026-05-20 22:54:47 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        boolean result = false;
+        for(int i = 0; i < nums.length; i++){
+            for(int y=i+1; y < nums.length; y++){
+                if(nums[i] == nums[y]){
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+}
